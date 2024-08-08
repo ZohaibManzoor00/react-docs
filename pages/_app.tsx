6fdc08e0946feff199416b7971257a1191234f57
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-08-08 15:25:23 -0400

    hot fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,11 @@ import "../custom.css";
 import Head from "next/head";
 import { SSRProvider } from "@react-aria/ssr";
 
-// Shim requestIdleCallback in Safari
-if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
-  window.requestIdleCallback = (fn) => setTimeout(fn, 1);
-  window.cancelIdleCallback = (e) => clearTimeout(e);
-}
+// // Shim requestIdleCallback in Safari
+// if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
+//   window.requestIdleCallback = (fn) => setTimeout(fn, 1);
+//   window.cancelIdleCallback = (e) => clearTimeout(e);
+// }
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
